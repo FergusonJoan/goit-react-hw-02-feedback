155ff4138b61a2d32b319a514eff5ca7b659d90b
Joan <prosandcons500@gmail.com>
2023-04-23 19:00:46 +0300

    fix styles

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export const Section = ({ title, children }) => {
   return (
     <>
-      <h2>{title}</h2>
+      <h2
+        style={{
+          margin: '15px 0',
+        }}
+      >
+        {title}
+      </h2>
       {children}
     </>
   );
